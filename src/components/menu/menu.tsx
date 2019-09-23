23a2dab05aa8f1d01ca1a1b9e21ed4b0a043f925
Anders Hansen <pareion12@gmail.com>
2019-09-23 20:26:15 +0200

    Færdig gjort bubblesort og start, stop og restart

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,71 +1,95 @@
-import React, { useState, SetStateAction, useEffect } from "react";
-import { Button } from "office-ui-fabric-react";
+import React, { useState } from "react";
+import { Button, Label } from "office-ui-fabric-react";
 import "./menu.css";
 import List from "./list";
 
 const Menu: React.FC = () => {
-  const [timer, setTimer] = useState({ seconds: 0 });
+  const [started, setStarted] = useState(false);
+  const [interval, setInter] = useState();
 
   const [list, setList] = useState({
     numbers: Array.from({ length: 40 }, () => Math.floor(Math.random() * 40))
   });
 
-  // Hvorfor unmounter den, den efter den har kørt en gang?
-  useEffect(() => {
-    function time() {
-      setTimer({ seconds: timer.seconds++ });
-      console.log(timer);
-    }
+  const [algorithm, setAlgorithm] = useState("None");
 
-    setInterval(time, 1000, timer.seconds);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    function bubblesort() {
-      const length = list.numbers.length;
-      for (let i = 1; i < length; i++) {
-        if (list.numbers[i - 1] > list.numbers[i]) {
-          let temp = list.numbers[i - 1];
-          list.numbers[i - 1] = list.numbers[i];
-          list.numbers[i] = temp;
-          setList({ numbers: list.numbers });
-          break;
-        }
+  function bubblesort() {
+    const length = list.numbers.length;
+    for (let i = 1; i < length; i++) {
+      if (list.numbers[i - 1] > list.numbers[i]) {
+        let temp = list.numbers[i - 1];
+        list.numbers[i - 1] = list.numbers[i];
+        list.numbers[i] = temp;
+        setList({ numbers: list.numbers });
+        break;
       }
     }
-    setInterval(bubblesort, 200, list);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }
+  function startAlgorithm(algorithm: string) {
+    if (!started) {
+      setStarted(true);
+      switch (algorithm) {
+        case "Bubblesort": {
+          setInter(setInterval(bubblesort, 100, list));
+          break;
+        }
+        case "Quicksort": {
+          break;
+        }
+        case "Mergesort": {
+          break;
+        }
+        default:
+          break;
+      }
+    }
+  }
 
   return (
     <div className="scrollmenu">
-      <p>Timer: {timer.seconds}</p>
-      <Button>Bubblesort</Button>
-      <Button>Quicksort</Button>
-      <Button>Mergesort</Button>
-      <Button onClick={() => {}}>Start sort</Button>
-
+      <Button
+        onClick={() => {
+          setAlgorithm("Bubblesort");
+          clearTimeout(interval);
+          setStarted(false);
+        }}
+      >
+        Bubblesort
+      </Button>
+      <Button disabled>Quicksort</Button>
+      <Button disabled>Mergesort</Button>
+      <Button
+        onClick={() => {
+          startAlgorithm(algorithm);
+        }}
+      >
+        Start
+      </Button>
+      <Button
+        onClick={() => {
+          clearTimeout(interval);
+          setStarted(false);
+        }}
+      >
+        Stop
+      </Button>
+      <Button
+        onClick={() => {
+          clearTimeout(interval);
+          setList({
+            numbers: Array.from({ length: 40 }, () =>
+              Math.floor(Math.random() * 40)
+            )
+          });
+          setStarted(false);
+        }}
+      >
+        Restart
+      </Button>
+      <Label>Algorithm: {algorithm}</Label>
       <List numbers={list.numbers} />
     </div>
   );
 };
-
-function BubbleSort(
-  numbersState: number[],
-  setNumbers: React.Dispatch<SetStateAction<number[]>>
-) {
-  console.log(numbersState);
-  const numbers = numbersState.slice();
-  const length = numbers.length;
-  for (let i = 1; i < length; i++) {
-    if (numbers[i - 1] > numbers[i]) {
-      let temp = numbers[i - 1];
-      numbers[i - 1] = numbers[i];
-      numbers[i] = temp;
-      setNumbers(numbers);
-    }
-  }
-}
 
 export default Menu;
